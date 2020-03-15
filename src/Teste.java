0332d54833bafe9975dfcd249868c906a3fa4822
Usuário <Usurio@DESKTOP-S6UICUT>
2020-03-15 14:07:10 -0300

    Inicial

---
+++ src/Teste.java
@@ -0,0 +1,48 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Teste {
+
+   /* public static void main(String[] args) {
+        PancakeHouseMenu phm = new PancakeHouseMenu();
+        DinerMenu dm = new DinerMenu();
+        LancheDaTardeMenu ltm = new LancheDaTardeMenu();
+        Garconete garconete = new Garconete(phm, dm, ltm);
+        garconete.printMenu();
+    }*/
+    
+    public static void main(String[] args) {
+        PancakeHouseMenu phm = new PancakeHouseMenu();
+        DinerMenu dm = new DinerMenu();
+        LancheDaTardeMenu ltm = new LancheDaTardeMenu();
+        Garconete g = null;
+        String menu = "1-Exibir menu geral\n2-Exibir menu de café da manhã\n3-Exibir menu de jantar\n4-Exibir menu de lanche da tarde\n";
+        
+        int opcaoCardapio = Integer.parseInt(JOptionPane.showInputDialog(menu));
+        switch(opcaoCardapio){
+            case 1: 
+                g = new Garconete(phm, dm, ltm);
+                g.printMenu();
+                break;
+            case 2:
+                g = new Garconete(phm);
+                g.printMenuPH();
+                break;
+            case 3:
+                g = new Garconete(dm);
+                g.printMenuDiner();
+                break;
+            case 4:
+                g = new Garconete(ltm);
+                g.printMenuTarde();
+                break;
+                
+        }
+        
+        
+        
+        //garconete.printMenu();
+    }
+    
+}
